3e7780a490b0dec87e09066d6a2fa9ca9446af58
drtee-jtb <mine4dot@gmail.com>
2026-05-25 20:06:51 +0000

    Flask-Render

--- src/app.py
+++ src/app.py
@@ -2,7 +2,11 @@ from flask import Flask, request, render_template
 from pickle import load
 
 app = Flask(__name__)
-model = load(open("../models/decision_tree_classifier_default_42.sav", "rb"))
+model = load(open("/workspaces/flask-render-integration/models/decision_tree_classifier_default_42.sav", "rb"))
+# Compatibility for models trained with older scikit-learn versions.
+if not hasattr(model, "monotonic_cst"):
+    model.monotonic_cst = None
+
 class_dict = {
     "0": "Iris setosa",
     "1": "Iris versicolor",
@@ -13,15 +17,15 @@ class_dict = {
 def index():
     if request.method == "POST":
         
-        # Obtain values from form
         val1 = float(request.form["val1"])
         val2 = float(request.form["val2"])
         val3 = float(request.form["val3"])
         val4 = float(request.form["val4"])
         
-        data = [[val1, val2, val3, val4]]
+        # Model expects: sepal length, sepal width, petal length, petal width.
+        data = [[val4, val3, val2, val1]]
         prediction = str(model.predict(data)[0])
-        pred_class = class_dict[prediction]
+        pred_class = class_dict.get(prediction, prediction)
     else:
         pred_class = None
     
